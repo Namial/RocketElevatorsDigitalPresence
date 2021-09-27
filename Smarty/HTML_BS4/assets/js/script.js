f87be789867ead89158ffcc9d15ef8b9e86b3857
Namial <91019487+Namial@users.noreply.github.com>
2021-09-27 15:46:16 -0400

    premier commit

---
+++ Smarty/HTML_BS4/assets/js/script.js
@@ -0,0 +1,32 @@
+function select_building() {
+    if()
+    document.querySelector("list_residential").style.display = "block";
+    else
+    document.querySelector("list_residential")
+    document.querySelector("list_residential")
+    document.querySelector("list_residential") 
+   }
+
+
+
+
+/* --commercial--
+number-of-elevators = total
+
+--residential--
+number-of-apartments/number-of-floors = moyenne app par etage
+one elevator each 6 app
+if +20app = 2 column
+
+--corporate/hybrid--
+maximum-occupancy*number-of-floors+basement = total-number-of-people
+total-number-of-people/1000 = number-of-elevator
+number-of-floor/20 = number-of-culomn
+number-of-elevator/number-of-culomn = moyenne-elevator-per-culomn
+number-of-elevator=moyenne-elevator-per-culomn*number-of-culomn
+
+--price--
+standard = 7565 + 10% fees
+premium = 12345 + 13% fees
+excelius = 15400 + 16% fees
+*/
